afeccdd2b591024787cdf1fe126ba8a8e5030ebb
Tatyana Ezhova <Tatyana.Ezhova@waveaccess.global>
2022-11-21 15:01:31 +0300

    Added bottom navigation view

--- app/src/main/java/com/example/jetpacknavigationsample/MainActivity.kt
+++ app/src/main/java/com/example/jetpacknavigationsample/MainActivity.kt
@@ -3,14 +3,18 @@ package com.example.jetpacknavigationsample
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.example.jetpacknavigationsample.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         val navHostFragment =
-            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+            supportFragmentManager.findFragmentById(R.id.navHostFragment) as NavHostFragment
         val navController = navHostFragment.navController
+        binding.bottomNavigationView.setupWithNavController(navController)
     }
 }
